c53778da6818409dcdbbd6537ac01f31154809b7
Laura Ringlstetter <ringlstetterlaura@gmail.com>
2025-07-23 20:18:09 +0200

    Ci-Farbe geändert, Timeline responsive, Animations (JavaScript)

--- js/animations.js
+++ js/animations.js
@@ -1,7 +1,26 @@
+//Für die fixe Navigationsleiste
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 0) {
-      document.body.classList.add('scrolled');
-    } else {
-      document.body.classList.remove('scrolled');
-    }
+  if (window.scrollY > 0) {
+    document.body.classList.add('scrolled');
+  } else {
+    document.body.classList.remove('scrolled');
+  }
+});
+
+//Animationseffekte
+document.addEventListener("DOMContentLoaded", function () {
+  const elements = document.querySelectorAll(".intro");
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target); // Optional: nur einmal beobachten
+      }
+    });
+  }, {
+    threshold: 0.1 // 10% des Elements muss sichtbar sein
   });
+
+  elements.forEach(el => observer.observe(el));
+});
